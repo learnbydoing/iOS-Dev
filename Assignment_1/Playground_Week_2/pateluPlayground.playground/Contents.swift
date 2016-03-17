52d487a80f03c71f6afac4f1d0d31694cfac25b9
Urvi Patel <urvipatel@MacBook-Pro-2.local>
2016-03-16 20:27:20 -0500

    Add P1

---
+++ Assignment_1/Playground_Week_2/pateluPlayground.playground/Contents.swift
@@ -0,0 +1,17 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+print(str)
+
+var a = 4 + 5;
+
+print(a);
+
+var b = "Hi"
+
+var c = "Hi"
+
+print(b==c)
